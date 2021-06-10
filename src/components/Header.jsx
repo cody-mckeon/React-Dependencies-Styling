13fa42218369c6865a8a6ba82cfd8193e016d16a
cody-mckeon <codymckeon@gmail.com>
2021-06-09 19:40:01 -0700

    Material UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import LocalLibraryIcon from "@material-ui/icons/LocalLibrary";
 
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1>
+        <LocalLibraryIcon />
+        Keeper
+      </h1>
     </header>
   );
 }
